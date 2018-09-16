5e87eadb80737bbe318c5e33bd4e93ada482a0e6
Max von Hippel <maxvonhippel1996@gmail.com>
2018-09-15 22:17:00 -0700

    all fixed (fixxed? idk)

--- scripts/daisy.js
+++ scripts/daisy.js
@@ -81,7 +81,7 @@ var color = d3.scale.category20c();
 var svg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height)
-	.append("g")
+  .append("g")
     .attr("transform", "translate(" + width / 2 + "," + (height / 2 + 10) + ")");
 
 var partition = d3.layout.partition()
@@ -93,49 +93,50 @@ var arc = d3.svg.arc()
     .innerRadius(function(d) { return Math.max(0, y(d.y)); })
     .outerRadius(function(d) { return Math.max(0, y(d.y + d.dy)); });
 
-d3.json(thedata, function(error, root) {
-  var g = svg.selectAll("g")
-      .data(partition.nodes(root))
-    .enter().append("g");
 
-  var path = g.append("path")
-    .attr("d", arc)
-    .style("fill", function(d) { return color((d.children ? d : d.parent).name); })
-    .on("click", click);
+root = thedata[0];
+var g = svg.selectAll("g")
+    .data(partition.nodes(root))
+  .enter().append("g");
 
-  var text = g.append("text")
-    .attr("transform", function(d) { return "rotate(" + computeTextRotation(d) + ")"; })
-    .attr("x", function(d) { return y(d.y); })
-    .attr("dx", "6") // margin
-    .attr("dy", ".35em") // vertical-align
-    .on("click", click)
-    .text(function(d) { return d.name; });
+var path = g.append("path")
+  .attr("d", arc)
+  .style("fill", function(d) { return color((d.children ? d : d.parent).name); })
+  .on("click", click);
 
-  function click(d) {
-  	if (d.url != null) {
-  		location.href = d.url;
-  	}
-  	// fade out all text elements
-  	text.transition().attr("opacity", 0);
+var text = g.append("text")
+  .attr("transform", function(d) { return "rotate(" + computeTextRotation(d) + ")"; })
+  .attr("x", function(d) { return y(d.y); })
+  .attr("dx", "6") // margin
+  .attr("dy", ".35em") // vertical-align
+  .on("click", click)
+  .text(function(d) { return d.name; });
 
-  	path.transition()
-  	.duration(750)
-  	.attrTween("d", arcTween(d))
-  	.each("end", function(e, i) {
-  	        // check if the animated element's data e lies within the visible angle span given in d
-  	        if (e.x >= d.x && e.x < (d.x + d.dx)) {
-
-  				var arcText = d3.select(this.parentNode).select("text");
-  				// fade in the text element and recalculate positions
-  				arcText.transition().duration(750)
-  				.attr("opacity", 1)
-  				.attr("transform", function() { return "rotate(" + computeTextRotation(e) + ")" })
-  				.attr("x", function(d) { return y(d.y); });
-  	        }
-  	});
+function click(d) {
+  if (d.url != null) {
+    location.href = d.url;
   }
+  // fade out all text elements
+  text.transition().attr("opacity", 0);
 
-});
+  path.transition()
+  .duration(750)
+  .attrTween("d", arcTween(d))
+  .each("end", function(e, i) {
+          // check if the animated element's data e lies within the visible angle span given in d
+          if (e.x >= d.x && e.x < (d.x + d.dx)) {
+
+        var arcText = d3.select(this.parentNode).select("text");
+        // fade in the text element and recalculate positions
+        arcText.transition().duration(750)
+        .attr("opacity", 1)
+        .attr("transform", function() { return "rotate(" + computeTextRotation(e) + ")" })
+        .attr("x", function(d) { return y(d.y); });
+          }
+  });
+}
+
+
 
 d3.select(self.frameElement).style("height", height + "px");
 
